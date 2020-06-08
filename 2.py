eac3a8d0972656db314fd77f5e8d9bb8d5bb6054
mingz <305603665@qq.com>
2020-06-08 18:05:56 +0800

    demo

---
+++ 2.py
@@ -0,0 +1,22 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+def drawFunc():
+    glClear(GL_COLOR_BUFFER_BIT)
+    #glRotatef(1, 0, 1, 0)
+    # glutWireTeapot(0.5)
+    # glutWireCube(1)
+    # glutWireOctahedron()
+    # glutWireSierpinskiSponge(0.1)
+    # glutSolidTeapot(0.5)
+    # glutSolidTorus(0.5, 0.5, 5, 10)
+    glFlush()
+
+glutInit()
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA)
+glutInitWindowSize(400, 400)
+glutCreateWindow("First")
+glutDisplayFunc(drawFunc)
+#glutIdleFunc(drawFunc)
+glutMainLoop()
